469322e1823dff5ff0e79d4aad6dd989b52792ec
Gnusin Pavel <pavel.sin.gnusin@gmail.com>
2020-03-30 11:04:24 +0300

    cache future object

--- functions/src/main/java/org/sample/functions/CacheComputer.java
+++ functions/src/main/java/org/sample/functions/CacheComputer.java
@@ -8,10 +8,10 @@ import java.util.function.Function;
 
 public class CacheComputer<K, V> implements Computing<K, V> {
 
-    private final ConcurrentMap<K, V> cache = new ConcurrentHashMap<>();
+    private final ConcurrentMap<K, Future<V>> cache = new ConcurrentHashMap<>();
 
     @Override
     public Future<V> compute(K key, Function<K, V> fn) {
-        return CompletableFuture.supplyAsync(() -> cache.computeIfAbsent(key, fn));
+        return cache.putIfAbsent(key, CompletableFuture.supplyAsync(() -> fn.apply(key)));
     }
 }
